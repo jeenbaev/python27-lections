a0c33e12ba5540f9a104e7a2a67cc9fe615ee5fc
Bekbol <jeenbaev.bekbol@gmail.com>
2023-02-21 17:23:54 +0600

    built-in-functions.py

--- basics/test.py
+++ basics/test.py
@@ -3,10 +3,12 @@
 # inp3 = int(input())
 
 # oellH 
-string = 'Hello'
-# print(string[-1] + string[1] + string[2])
-print(string[-1] + string[1:-1] + string[0])
+# string = 'Hello'
+# # print(string[-1] + string[1] + string[2])
+# print(string[-1] + string[1:-1] + string[0])
 
 
-radius = 6
-print(radius.math.pi )
+# radius = 6
+# print(radius.math.pi )
+
+
